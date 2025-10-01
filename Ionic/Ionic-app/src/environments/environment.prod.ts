29b22f5fc8ba931cd9d023412dfec4b612342271
DanielBata <batahernandez12345@gmail.com>
2025-10-01 14:52:46 -0500

    FiltroPorductoMejorado

--- Ionic/Ionic-app/src/environments/environment.prod.ts
+++ Ionic/Ionic-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // Prod (backend público con HTTPS válido)
 export const environment = {
   production: true,
-  apiUrl: 'http:/192.168.20.28/:5253/api/v1/'
+  apiUrl: 'http:/172.30.6.51/:5253/api/v1/'
 };
