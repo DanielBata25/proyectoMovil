1248ae4069a3c149fbddd84af1324f69b9cebbd6
DanielBata <batahernandez12345@gmail.com>
2025-11-26 19:19:50 -0500

    ConfiguracionCarrusel

--- Ionic/Ionic-app/src/environments/environment.prod.ts
+++ Ionic/Ionic-app/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // Prod (backend público con HTTPS válido)
 export const environment = {
   production: true,
-  apiUrl: 'https://lawana-unhale-sorrowfully.ngrok-free.dev/api/v1/',
+  apiUrl: 'https://web20251124155458-chefafacbmaxerdc.canadacentral-01.azurewebsites.net/api/v1/',
   /** En build nativo coincide con la URL real (no se usa proxy) */
   apiUrlBrowser: '',
 };         
